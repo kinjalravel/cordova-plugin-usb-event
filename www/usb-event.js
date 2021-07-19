cd4b1b2f2101d353b7a662e71f42df0adecdd0b2
kinjalravel <kinjalraval18591@gmail.com>
2021-07-19 20:29:54 +0530

    Update usb-event.js

--- www/usb-event.js
+++ www/usb-event.js
@@ -1,64 +1,75 @@
+cordova.define("cordova-plugin-usb-event.UsbEvent", function(require, exports, module) {
 var exec = require('cordova/exec');
 
-/**
- * list USB devices.
- */
-exports.listDevices = function(success, error, options) {
-    exec(success, error, 'UsbEvent', 'listDevices', [options]);
-};
+ /**
+  * list USB devices.
+  */
+ exports.listDevices = function(success, error, options) {
+     exec(success, error, 'UsbEvent', 'listDevices', [options]);
+ };
 
-/**
- * Check callback is already exists.
- */
-exports.existsRegisteredCallback = function(success, error) {
-    exec(success, error, 'UsbEvent', 'existsRegisteredCallback', []);
-};
+ /**
+  * Check callback is already exists.
+  */
+ exports.existsRegisteredCallback = function(success, error) {
+     exec(success, error, 'UsbEvent', 'existsRegisteredCallback', []);
+ };
 
-/**
- * Register USB attached and detached event callback.
- */
-exports.registerEventCallback = function(success, error, options) {
-    exec(success, error, 'UsbEvent', 'registerEventCallback', [options]);
-};
+ /**
+  * Register USB attached and detached event callback.
+  */
+ exports.registerEventCallback = function(success, error, options) {
+     exec(success, error, 'UsbEvent', 'registerEventCallback', [options]);
+ };
 
-/**
- * Clear registered callback.
- */
-exports.unregisterEventCallback = function(success, error, options) {
-    exec(success, error, 'UsbEvent', 'unregisterEventCallback', [options]);
-};
+ /**
+  * Clear registered callback.
+  */
+ exports.unregisterEventCallback = function(success, error, options) {
+     exec(success, error, 'UsbEvent', 'unregisterEventCallback', [options]);
+ };
 
-/**
- * Create file callback.
- */
-exports.createFileEventCallback = function(options, success, error) {
-    exec(success, error, 'UsbEvent', 'createFileEventCallback', [options]);
-};
+ /**
+  * Create file callback.
+  */
+ exports.createFileEventCallback = function(options, success, error) {
+     exec(success, error, 'UsbEvent', 'createFileEventCallback', [options]);
+ };
 
-/**
- * Delete file callback.
- */
-exports.deleteFileEventCallback = function(options, success, error) {
-    exec(success, error, 'UsbEvent', 'deleteFileEventCallback', [options]);
-};
+ /**
+  * Delete file callback.
+  */
+ exports.deleteFileEventCallback = function(options, success, error) {
+     exec(success, error, 'UsbEvent', 'deleteFileEventCallback', [options]);
+ };
 
-/**
- * Read file callback.
- */
-exports.readFileEventCallback = function(options, success, error) {
-    exec(success, error, 'UsbEvent', 'readFileEventCallback', [options]);
-};
+ /**
+  * Read file callback.
+  */
+ exports.readFileEventCallback = function(options, success, error) {
+     exec(success, error, 'UsbEvent', 'readFileEventCallback', [options]);
+ };
 
-/**
- * Write file callback.
- */
-exports.writeFileEventCallback = function(options, success, error) {
-    exec(success, error, 'UsbEvent', 'writeFileEventCallback', [options]);
-};
+  /**
+   * Read file bytes callback.
+   */
+  exports.readFileBytesEventCallback = function(options, success, error) {
+      exec(success, error, 'UsbEvent', 'readFileBytesEventCallback', [options]);
+  };
 
-/**
- * check file callback.
- */
-exports.fileExistFileEventCallback = function(options, success, error) {
-    exec(success, error, 'UsbEvent', 'fileExistFileEventCallback', [options]);
-};
+
+ /**
+  * Write file callback.
+  */
+ exports.writeFileEventCallback = function(options, success, error) {
+     exec(success, error, 'UsbEvent', 'writeFileEventCallback', [options]);
+ };
+
+ /**
+  * check file callback.
+  */
+ exports.fileExistFileEventCallback = function(options, success, error) {
+     exec(success, error, 'UsbEvent', 'fileExistFileEventCallback', [options]);
+ };
+
+});
